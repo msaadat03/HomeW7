e5701fb20d5096332c3f1413a2d5528806654230
msaadat03 <saadatam@code.edu.az>
2022-06-22 00:54:34 +0300

    HomeW7

---
+++ HomeW7/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace HomeW7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                ReversingHomeW7("Hi, guys!");
+            }
+            static void ReversingHomeW7(string sentence)
+            {
+                string str;
+                str = string.Empty;
+                for (int i = sentence.Length - 1; i >= 0; i--)
+                {
+                    str += sentence[i];
+                }
+                Console.WriteLine(str);
+            }
+        }
+    }
+}
